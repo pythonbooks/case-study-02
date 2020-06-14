348c43716566b5ccbf8c8214ed2852e748e381de
Jay Wang, D.Sc <wcj365@gmail.com>
2020-06-14 11:27:15 -0400

    Update order_pizza.py

--- order_pizza.py
+++ order_pizza.py
@@ -1,4 +1,8 @@
 #/usr/bin/bash python3
+"""
+This script use the PizzaOrder class defined in a separate file pizza_order.py to generate 1o instances of pizza orders. 
+The faker package is used to generate fake names and dates. 
+"""
 
 import operator
 from faker import Faker
@@ -6,12 +10,12 @@ from random import randint
 
 import pizza_order
 
-fake= Faker()
+fake = Faker()
 
-orders = []              # initialize a list to hold the simulated pizza orders
+orders = []              # initialize a list to hold the simulated pizza order objects
 
 
-# Simulate 10 pizza orders
+# Generate 10 pizza orders
 for i in range(10):  
   
     # generate one fake pizza order at a time 
@@ -23,9 +27,11 @@ for i in range(10):
     )
     
     orders.append(order)   
+    
 
 # sort the orders by the order amount
 orders_sorted  = sorted(orders, key=operator.attrgetter("order_amount"))
+
 
 for order in orders_sorted:
     print("Order: "     \
